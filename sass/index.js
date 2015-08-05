96e53c47d3772adb1bcf8cb0c1bb02e7da605e1f
inuscript <inuscript@gmail.com>
2015-08-06 08:51:11 +0900

    add

--- sass/index.js
+++ sass/index.js
@@ -1,23 +1,10 @@
-var sassMap = require("./map")
-var sassTemplates = require("./templates")
-var partialNames = require("./partials") 
+var partials = require("./partials")
 
-var order = partialNames.map(function(p){
-  return p.name
-})
-var allPartials = order
-
-module.exports = function(map, includes){
-  if(includes === undefined){
-    includes = allPartials
-  }
-  var templates = sassTemplates()
-  templates["map"] = map
-
-  var scssContents = []
-  return order.map(function(partial){
-    return templates[partial] || null
-  }).filter(function(scss){
-    return scss !== null
-  }).join("\n\n")
+module.exports = function(map){
+  var scss = [
+    partials.map(map),
+    partials.mixin(),
+    partials.loader()
+  ]
+  return scss.join("\n\n")
 }
