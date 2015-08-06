1e92c4c9ffb00e62529cfb8ec8c056a7015b5ba7
inuscript <inuscript@gmail.com>
2015-08-06 09:30:59 +0900

    remove render

--- render/scss.js
+++
@@ -1,8 +0,0 @@
-var sassMap = require("../sass/map")
-var sassBuilder = require("../sass/")
-
-module.exports = function(data, options){
-  var map = sassMap(data, options.fontName, options.asDefault)
-  var scss = sassBuilder(map)
-  return scss
-}
