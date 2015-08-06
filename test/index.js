0d8a9b44b83d59443f105d86aba85ecc70e0bf03
inuscript <inuscript@gmail.com>
2015-08-06 20:44:39 +0900

    css compile test

--- test/index.js
+++ test/index.js
@@ -2,12 +2,21 @@ var iconfontSass = require("../index")
 var assert = require("assert")
 var fs = require("fs")
 var fixtrueGlyphs = require("./fixture/codepoint.json")
+var compiler = require("node-sass")
 
 
 describe("sass rendering", function(){
-  it("should rendering", function(){
-    var expect = fs.readFileSync("./test/fixture/output.scss", "utf-8")
-    var result = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font")
-    assert.equal(expect, result);
+  // it("should rendering", function(){
+  //   var expect = fs.readFileSync("./test/fixture/output.scss", "utf-8")
+  //   var result = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font")
+  //   assert.equal(expect, result);
+  // })
+  it("should compilable", function(){
+    var expect = fs.readFileSync("./test/fixture/output.css", "utf-8")
+    var scss = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font")
+    var css = compiler.renderSync({
+      data: scss
+    }).css.toString()
+    assert.equal(expect, css)
   })
 })
