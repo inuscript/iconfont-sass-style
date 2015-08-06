fb35cfb4adc131cb83ec0aa5dd4c6a3ebab90d1d
inuscript <inuscript@gmail.com>
2015-08-06 20:51:08 +0900

    fix test

--- test/index.js
+++ test/index.js
@@ -6,14 +6,16 @@ var compiler = require("node-sass")
 
 
 describe("sass rendering", function(){
-  // it("should rendering", function(){
-  //   var expect = fs.readFileSync("./test/fixture/output.scss", "utf-8")
-  //   var result = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font")
-  //   assert.equal(expect, result);
-  // })
+  it("should rendering", function(){
+    var expect = fs.readFileSync("./test/fixture/output.scss", "utf-8")
+    var result = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font")
+    assert.equal(expect, result)
+  })
   it("should compilable", function(){
     var expect = fs.readFileSync("./test/fixture/output.css", "utf-8")
-    var scss = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font")
+    var scss = iconfontSass(fixtrueGlyphs, "myFont", "/path/to/font", {
+      iconPrefix: ".myIcon-"
+    })
     var css = compiler.renderSync({
       data: scss
     }).css.toString()
