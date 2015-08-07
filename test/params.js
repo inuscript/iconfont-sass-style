ee5e12ba14b37f0c5b963ce64ccbcace09d92a6d
inuscript <inuscript@gmail.com>
2015-08-07 19:54:25 +0900

    add test

--- test/params.js
+++ test/params.js
@@ -14,4 +14,14 @@ describe("params", function(){
 
     assert.deepEqual(expect, result)
   })
+  it("shoud return object when not throw param", function(){
+    var result = params(fixtrueGlyphs, "foo", "/path/to/baz")
+    var expect = { 
+      fontName: '"foo"',
+      fontPath: '"/path/to/baz"',
+      glyphs: { account: '"\\E001"', 'arrow-down': '"\\E002"' } 
+    }
+
+    assert.deepEqual(expect, result)
+  })
 })
